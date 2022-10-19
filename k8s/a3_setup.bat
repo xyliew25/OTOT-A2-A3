13da0f736c1998e1e8ba7aabb014fb3442277ffa
xyliew25 <e0550381@u.nus.edu>
2022-10-19 13:37:25 +0800

    Create metrics server, hpa and zone aware deployment

---
+++ k8s/a3_setup.bat
@@ -0,0 +1,11 @@
+:: Create metrics server
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml 
+kubectl -nkube-system edit deploy/metrics-server
+:: add a flag --kubelet-insecure-tls to deployment.spec.containers[].args[]
+kubectl -nkube-system rollout restart deploy/metrics-server
+
+:: Create hpa
+kubectl apply -f k8s/manifests/k8s/backend-hpa.yaml
+
+:: Create zone aware deployment
+kubectl apply -f k8s/manifests/k8s/backend-deployment-zone-aware.yaml
